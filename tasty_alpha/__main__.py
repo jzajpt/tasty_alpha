de2b78e52e04029885726bc380852279acd4b0f5
Jiri Zajpt <jzajpt@users.noreply.github.com>
2019-03-30 21:12:52 +0100

    Add subcommands

--- tasty_alpha/__main__.py
+++ tasty_alpha/__main__.py
@@ -1,31 +1,44 @@
 import click
-from .bar_generator import CSVTradeProcessor, CSVBarWriter
+from .csv_io import CSVTradeProcessor, CSVBarWriter
 from .bars import DollarBars, TickBars, VolumeBars
 from .feed import FeedProcessor
+
+def new_bars(bar, threshold):
+    if bar == 'tick':
+        return TickBars(threshold)
+    elif bar == 'dollar':
+        return  DollarBars(threshold)
+    elif bar == 'volume':
+        return  VolumeBars(threshold)
+    else:
+        raise Exception(f'Invalid bar: {bar}')
+
+
+@click.group()
+def cli():
+    pass
 
 @click.command()
 @click.option('--file', '-f', default='', help='Input CSV file')
 @click.option('--bar', '-b', default='tick', help='Bar type')
 @click.option('--threshold', '-t', type=int, default='', help='Bar type')
-def cli(file, bar, threshold):
-    bars = None
-    if bar == 'tick':
-        bars = TickBars(threshold)
-    elif bar == 'dollar':
-        bars =  DollarBars(threshold)
-    elif bar == 'volume':
-        bars =  VolumeBars(threshold)
-    else:
-        print(f'Invalid bar: {bar}')
-        return
+def backtest(file, bar, threshold):
+    bars = new_bars(bar, threshold)
     bar_writer = CSVBarWriter('output.csv')
-    # trade_processor = CSVTradeProcessor(file)
-    trade_processor = FeedProcessor()
+    trade_processor = CSVTradeProcessor(file)
     trade_processor.run()
 
 @click.command()
-def feed(file):
-    print('feed')
+@click.option('--bar', '-b', default='tick', help='Bar type')
+@click.option('--threshold', '-t', type=int, default='', help='Bar type')
+def livefeed(bar, threshold):
+    bars = new_bars(bar, threshold)
+    trade_processor = FeedProcessor()
+    trade_processor.run()
+
+
+cli.add_command(backtest)
+cli.add_command(livefeed)
 
 if __name__ == '__main__':
     cli()
